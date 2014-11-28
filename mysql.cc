9a6f125d26de6a4bc603e58674289db79902b376
yoku0825 <yoku0825@gmail.com>
2014-11-28 17:03:53 +0900

    Add my copyright.

--- mysql.cc
+++ mysql.cc
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2014, yoku0825. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
